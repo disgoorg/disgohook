8c40ba0cf071a08c225afee49636aa2aeb366819
TopiSenpai <tobi@strasserhof.com>
2021-04-23 20:27:35 +0200

    fixed stuff lol

--- disgohook.go
+++ disgohook.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/DisgoOrg/disgohook/api"
 	"github.com/DisgoOrg/disgohook/internal"
+	"github.com/DisgoOrg/log"
 )
 
-func NewWebhookByToken(webhookToken string) (api.Webhook, error) {
+func NewWebhookByToken(webhookToken string, logger log.Logger) (api.Webhook, error) {
 	webhookTokenSplit := strings.SplitN(webhookToken, "/", 2)
 	if len(webhookTokenSplit) != 2 {
 		return nil, api.ErrMalformedWebhookToken
 	}
-	return internal.NewWebhookImpl(webhookTokenSplit[0], webhookTokenSplit[1]), nil
+	return internal.NewWebhookImpl(nil, logger, webhookTokenSplit[0], webhookTokenSplit[1]), nil
 }
 
 func NewWebhookByID(id string, token string) (api.Webhook, error) {
--- go.mod
+++ go.mod
@@ -3,5 +3,5 @@ module github.com/DisgoOrg/disgohook
 go 1.16
 
 require (
-	github.com/DisgoOrg/log v1.0.3 //indirect
+	github.com/DisgoOrg/log v1.0.3
 )
